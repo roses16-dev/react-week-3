911100dbc2aa830c646212aff76089b6b8c81d47
seongwoo <seongwoo91@naver.com>
2022-10-15 23:57:08 +0900

    댓글삭제버그있음

--- src/components/postdetail/Comment.jsx
+++ src/components/postdetail/Comment.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { __getComments } from "../../redux/modules/commentSlice";
 import { useState } from "react";
 import axios from "axios";
+import "./comment.css";
 
 const Comment = ({ id }) => {
   const dispatch = useDispatch();
@@ -22,7 +23,8 @@ const Comment = ({ id }) => {
     <>
       <div className="comment">
         <strong className="cmt_length">현재 댓글 {cmt.length} 개</strong>
-        {!loadingtest && cmt.map((e, i) => <CommentItem key={e.id} e={e} />)}
+        {!loadingtest &&
+          cmt.map((e, i) => <CommentItem key={e.id} e={e} id={id} />)}
         <CommentForm id={id} />
       </div>
     </>
@@ -31,26 +33,36 @@ const Comment = ({ id }) => {
 
 export default Comment;
 
-const CommentItem = ({ e }) => {
+const CommentItem = ({ e, id }) => {
+  console.log("아이딧", e.id);
+  //댓글 삭제하기
+  const delComment = () => {
+    axios
+      .delete(`http://localhost:3001/comments/${e.id}`)
+      .then(console.log("123"));
+  };
   return (
     <div className="cmtbox">
       <p className="cmt_name">{e.author}</p>
       <p className="cmt_desc">{e.content}</p>
       <button className="mod_btn">수정</button>
-      <button className="del_btn">삭제</button>
+      <button className="del_btn" onClick={delComment}>
+        삭제
+      </button>
     </div>
   );
 };
 
 const CommentForm = ({ id }) => {
+  const dispatch = useDispatch();
   const commentform = {
     userid: "",
     userpw: "",
     desc: "",
+    value: "",
   };
 
   const [comment, setCmt] = useState(commentform);
-
   const onCmtChangeHandler = (e) => {
     const { name, value } = e.target;
     setCmt({ ...comment, [name]: value });
@@ -58,16 +70,19 @@ const CommentForm = ({ id }) => {
   };
   console.log("id넘어오는지 확인", id);
 
+  //댓글 추가하기 요청
   const addComment = (e) => {
     e.preventDefault();
 
-    axios.post(`http://localhost:3001/comments/`, {
-      commentId: id,
-      id: Date.now() + "",
-      author: comment.userid,
-      password: comment.userpw,
-      content: comment.desc,
-    });
+    axios
+      .post(`http://localhost:3001/comments/`, {
+        commentId: id,
+        id: Date.now() + "",
+        author: comment.userid,
+        password: comment.userpw,
+        content: comment.desc,
+      })
+      .then(setCmt(commentform), dispatch(__getComments(id)));
   };
 
   return (
@@ -80,6 +95,7 @@ const CommentForm = ({ id }) => {
               className="cmt_id"
               placeholder="이름"
               name="userid"
+              value={comment.userid}
               onChange={onCmtChangeHandler}
             />
             <input
@@ -87,6 +103,7 @@ const CommentForm = ({ id }) => {
               className="cmt_pw"
               placeholder="비밀번호"
               name="userpw"
+              value={comment.userpw}
               onChange={onCmtChangeHandler}
             />
           </div>
@@ -95,6 +112,7 @@ const CommentForm = ({ id }) => {
               className="cmt_desc"
               name="desc"
               placeholder="댓글을 작성해주세요."
+              value={comment.desc}
               onChange={onCmtChangeHandler}
             />
           </div>
