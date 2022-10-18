368b818cffc8aabe8ad8a3083f3c3180a36e3bbc
Kim Minjoo <roses16.dev@gmail.com>
2022-10-18 17:12:10 +0900

    infinite scroll limit 변경

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -7,41 +7,41 @@ import useInfiniteScroll from "../hooks/useInfiniteScroll";
 import Header from "../components/header/Header";
 
 function List() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const FIRST_POST_LIMIT = 30;
-  const ADD_POST_LIMIT = 10;
-  const [isFetching, setIsFetching] = useInfiniteScroll(updateFunctionOnScroll);
-  const [pageNumber, setPageNumber] = useState(4);
+    const FIRST_POST_LIMIT = 20;
+    const ADD_POST_LIMIT = 10;
+    const [isFetching, setIsFetching] = useInfiniteScroll(updateFunctionOnScroll);
+    const [pageNumber, setPageNumber] = useState(2);
 
-  const posts = useSelector((state) => state.posts.posts);
+    const posts = useSelector((state) => state.posts.posts);
 
-  function updateFunctionOnScroll() {
-    try {
-      if (isFetching) {
-        dispatch(
-          __accruePostsByPage({ page: pageNumber, limit: ADD_POST_LIMIT })
-        );
-        setPageNumber(pageNumber + 1);
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsFetching(false);
+    function updateFunctionOnScroll() {
+        try {
+            if (isFetching) {
+                dispatch(
+                    __accruePostsByPage({ page: pageNumber, limit: ADD_POST_LIMIT })
+                );
+                setPageNumber(pageNumber + 1);
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsFetching(false);
+        }
     }
-  }
 
-  useEffect(() => {
-    dispatch(__getPosts({ page: 1, limit: FIRST_POST_LIMIT }));
-    updateFunctionOnScroll();
-  }, []);
+        useEffect(() => {
+        dispatch(__getPosts({ page: 1, limit: FIRST_POST_LIMIT }));
+        updateFunctionOnScroll();
+    }, []);
 
-  return (
-    <>
-      <Header />
-      <PostSummary post={posts} />
-    </>
-  );
+    return (
+        <>
+        <Header />
+        <PostSummary post={posts} />
+        </>
+    );
 }
 
 export default List;
