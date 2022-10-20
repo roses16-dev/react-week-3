3b124ddd6568400bdd2d5b829832444ba330490d
Kim Minjoo <roses16.dev@gmail.com>
2022-10-20 15:46:53 +0900

    test / 배포 환경에서 devtools off

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,9 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+
 import posts from "../modules/postsSlice";
 import comments from "../modules/commentSlice";
 
 const store = configureStore({
   reducer: { posts: posts, comments, comments },
-});
+}, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
